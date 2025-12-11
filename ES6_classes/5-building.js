392a96e20ef84d3baeacc49c1b11f2feee91362b
Priam <priamdmy@gmail.com>
2025-12-11 16:15:21 +0100

    [fix] new.target -> this.constructor

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -4,11 +4,11 @@ export default class Building {
       throw new TypeError('sqft must be a number');
     }
 
-    this._sqft = sqft;
-
-    if (new.target === Building && !new.target.evacuationWarningMessage) {
+    if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
+
+    this._sqft = sqft;
   }
 
   get sqft() {
