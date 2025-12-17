0168a69ba78c44dd91e33f236f590b9aaf4a04bd
Priam <priamdmy@gmail.com>
2025-12-17 11:03:54 +0100

    [first] output tests ok

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,10 @@
+import { uploadPhoto, createUser } from "./utils"
+
+export default async function handleProfileSignup() {
+  try {
+    const [photo, user] = await Promise.all([uploadPhoto(), createUser()]);
+    console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+  } catch (err) {
+    console.log("Signup system offline");
+  }
+}
