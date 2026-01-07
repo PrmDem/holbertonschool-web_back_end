98f7def5da30412c7fa796138297971132329a0f
Priam <priamdmy@gmail.com>
2026-01-07 15:16:54 +0100

    [first] output tests & linter ok

---
+++ Node_JS_basic/5-http.js
@@ -0,0 +1,23 @@
+const http = require('http');
+const countStudents = require('./3-read_file_async'); // Using past file as a module
+
+const app = http.createServer(async (req, res) => {
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write('This is the list of our students\n'); // Need to add \n manually with res.write
+
+    try {
+      const data = await countStudents(process.argv[2]); // path to file given in command line
+      res.end(data);
+    } catch (err) {
+      res.end(err.message);
+    }
+  }
+});
+
+app.listen(1245);
+
+module.exports = app;
