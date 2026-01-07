f43add2867eb76f1b27a17ab2c3937eed8ebdf20
Priam <priamdmy@gmail.com>
2026-01-07 10:51:12 +0100

    [first] output tests & eslint ok

---
+++ Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,33 @@
+const fs = require('node:fs/promises');
+
+async function countStudents(pathToFile) { // declare async
+  try {
+    const data = await fs.readFile(pathToFile, 'utf8'); // await + readFileSync -> readFile
+    const lines = data.trim().split('\n'); // Turn csv into Array obj, removing blank spaces
+    const students = lines.slice(1); // students are now separate from fields
+
+    console.log(`Number of students: ${students.length}\n`);
+
+    const lists = {};
+
+    students.forEach((stdn) => {
+      // eslint-disable-next-line no-unused-vars
+      const [first, _last, _age, field] = stdn.split(',');
+      if (!lists[field]) {
+        lists[field] = []; // add as 'lists' object properties, value is a list
+      }
+      lists[field].push(first); // adds first name to the corresponding list
+    });
+
+    for (const spec in lists) {
+      if (Object.hasOwn(lists, spec)) { // for eslint 'guard for in'
+        const specList = lists[spec]; // Getting field property's value for manipulation
+        console.log(`Number of students in ${spec}: ${specList.length}. List: ${specList.join(', ')}`);
+      }
+    }
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
